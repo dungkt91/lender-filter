d592ebb9390f08a3b4c225eb71c341f42687ddc8
Vu Van Dung <vandungktvn@gmail.com>
2020-03-28 00:54:52 +0700

    Keep images same size

--- src/CarImagesGallery.js
+++ src/CarImagesGallery.js
@@ -1,43 +1,44 @@
 import * as React from "react";
 import ImageGallery from 'react-image-gallery';
 import 'react-image-gallery/styles/css/image-gallery.css';
+import './CarImagesGallery.css';
 
 const images = [
     {
         original: 'https://www.dropbox.com/s/0xtbjp3gcm650fy/image_0.png?raw=1',
-        thumbnail: 'https://www.dropbox.com/s/0xtbjp3gcm650fy/image_0.png?raw=1',
+        thumbnail: 'https://www.dropbox.com/s/0xtbjp3gcm650fy/image_0.png?raw=1'
     },
     {
         original: 'https://www.dropbox.com/s/tuj84ho2up1vofh/image_1.png?raw=1',
-        thumbnail: 'https://www.dropbox.com/s/tuj84ho2up1vofh/image_1.png?raw=1',
+        thumbnail: 'https://www.dropbox.com/s/tuj84ho2up1vofh/image_1.png?raw=1'
     },
     {
         original: 'https://www.dropbox.com/s/1sa79eyy7og6t51/image_2.png?raw=1',
-        thumbnail: 'https://www.dropbox.com/s/1sa79eyy7og6t51/image_2.png?raw=1',
+        thumbnail: 'https://www.dropbox.com/s/1sa79eyy7og6t51/image_2.png?raw=1'
     },
     {
         original: 'https://www.dropbox.com/s/nfkkglb0odsu4te/image_3.png?raw=1',
-        thumbnail: 'https://www.dropbox.com/s/nfkkglb0odsu4te/image_3.png?raw=1',
+        thumbnail: 'https://www.dropbox.com/s/nfkkglb0odsu4te/image_3.png?raw=1'
     },
     {
         original: 'https://www.dropbox.com/s/2vq1cj46oot8kf2/image_4.png?raw=1',
-        thumbnail: 'https://www.dropbox.com/s/2vq1cj46oot8kf2/image_4.png?raw=1',
+        thumbnail: 'https://www.dropbox.com/s/2vq1cj46oot8kf2/image_4.png?raw=1'
     },
     {
         original: 'https://www.dropbox.com/s/qvo9jf2ufn0p4kt/image_5.png?raw=1',
-        thumbnail: 'https://www.dropbox.com/s/qvo9jf2ufn0p4kt/image_5.png?raw=1',
+        thumbnail: 'https://www.dropbox.com/s/qvo9jf2ufn0p4kt/image_5.png?raw=1'
     },
     {
         original: 'https://www.dropbox.com/s/covz2iuq9sz6oe2/image_6.png?raw=1',
-        thumbnail: 'https://www.dropbox.com/s/covz2iuq9sz6oe2/image_6.png?raw=1',
+        thumbnail: 'https://www.dropbox.com/s/covz2iuq9sz6oe2/image_6.png?raw=1'
     },
     {
         original: 'https://www.dropbox.com/s/861x4fx5nl3pdtr/image_7.png?raw=1',
-        thumbnail: 'https://www.dropbox.com/s/861x4fx5nl3pdtr/image_7.png?raw=1',
+        thumbnail: 'https://www.dropbox.com/s/861x4fx5nl3pdtr/image_7.png?raw=1'
     },
     {
         original: 'https://www.dropbox.com/s/rf2z9ksajhmjzjj/image_8.png?raw=1',
-        thumbnail: 'https://www.dropbox.com/s/rf2z9ksajhmjzjj/image_8.png?raw=1',
+        thumbnail: 'https://www.dropbox.com/s/rf2z9ksajhmjzjj/image_8.png?raw=1'
     },
 ];
 
