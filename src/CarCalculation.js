4d5de11cbd8867d6a3d1fd9f7725db0f27333074
Vu Van Dung <vandungktvn@gmail.com>
2020-03-28 10:07:44 +0700

    Put car description and car calculation inside a <Paper>, and set background for theses <Paper>

--- src/CarCalculation.js
+++ src/CarCalculation.js
@@ -3,6 +3,7 @@ import Table from "@material-ui/core/Table";
 import TableRow from "@material-ui/core/TableRow";
 import TableHead from "@material-ui/core/TableHead";
 import TableCell from "@material-ui/core/TableCell";
+import Paper from "@material-ui/core/Paper";
 
 export default class CarCalculation extends React.Component{
     constructor() {
@@ -28,33 +29,35 @@ export default class CarCalculation extends React.Component{
     }
     render(){
         return (
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        {this.calculationDetailsColumnHeaders.map(header =>(
-                         <TableCell>
-                             {header}
-                         </TableCell>
-                        ))}
-                    </TableRow>
-                </TableHead>
-                {this.calculationDetailsColumnValues.map(rowValues =>
-                            (
-                                <TableRow>
-                                    {
-                                        rowValues.map(rowValue =>
-                                            (
-                                                <TableCell>
-                                                    {rowValue}
-                                                </TableCell>
+            <Paper style={{padding:"20px", backgroundColor:"rgb(247, 248, 248)"}}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            {this.calculationDetailsColumnHeaders.map(header =>(
+                             <TableCell>
+                                 {header}
+                             </TableCell>
+                            ))}
+                        </TableRow>
+                    </TableHead>
+                    {this.calculationDetailsColumnValues.map(rowValues =>
+                                (
+                                    <TableRow>
+                                        {
+                                            rowValues.map(rowValue =>
+                                                (
+                                                    <TableCell>
+                                                        {rowValue}
+                                                    </TableCell>
+                                                )
                                             )
-                                        )
-                                    }
-                                </TableRow>
+                                        }
+                                    </TableRow>
+                                )
                             )
-                        )
-                    }
-            </Table>
+                        }
+                </Table>
+            </Paper>
         )
     }
 }
--- src/CarDescription.js
+++ src/CarDescription.js
@@ -3,23 +3,24 @@ import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 
 export default class CarDescription extends React.Component{
     render(){
         return (
-            <div>
+            <Paper style={{padding:"20px", backgroundColor:"rgb(247, 248, 248)"}}>
                 <Table>
                     {this.props.details.map(row => (
                         row.name == "title"?
                             (
                                 <TableRow>
-                                    <TableCell colSpan={2} style={{textAlign:"center"}}>{row.value}</TableCell>
+                                    <TableCell colSpan={2} style={{textAlign:"center"}}><b style={{fontSize:"1.2rem"}}>{row.value}</b></TableCell>
                                 </TableRow>
                             ):
                             (
                                 <TableRow>
                                     <TableCell style={{padding:"0px"}}>
-                                        {row.name}
+                                        <b>{row.name}</b>
                                     </TableCell>
                                     <TableCell style={{padding:"0px"}}>
                                         {row.value}
@@ -28,7 +29,7 @@ export default class CarDescription extends React.Component{
                             )
                     ))}
                 </Table>
-            </div>
+            </Paper>
         )
     }
 }
