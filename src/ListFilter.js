31a12083fbc4f31378f145bd80222f3f2d220118
Vu Van Dung <vandungktvn@gmail.com>
2020-06-20 10:03:32 +0700

    Add List Filter

---
+++ src/ListFilter.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import {Checkbox} from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+
+class ListFilter extends React.Component{
+    constructor(props) {
+        super(props)
+
+        let optionCheckedDict = {};
+
+        for(let option of this.props.options){
+            optionCheckedDict[option] = true;
+        }
+
+        this.state = {
+            'Select All': true,
+            ...optionCheckedDict
+        };
+
+        this.selectAll = this.selectAll.bind(this);
+        this.optionOnchange = this.optionOnchange.bind(this);
+    }
+
+    getTitle(){
+        return this.props.title;
+    }
+
+    getSelectedOptions(){
+        let selectedOptions = [];
+
+        for (let option of this.props.options){
+            if (this.state[option]){
+                selectedOptions.push(option);
+            }
+        }
+
+        return selectedOptions;
+    }
+
+
+    selectAll(event){
+        console.log('Select all');
+        let selectAllCheckbox = event.target;
+        let selectedCheckboxState = selectAllCheckbox.checked;
+        let newState = {...this.state};
+
+        for(let option of this.props.options){
+            if (selectAllCheckbox.checked){
+                newState[option] = true;
+            }else {
+                newState[option] = false;
+            }
+        }
+
+        newState['Select All'] = selectedCheckboxState;
+        this.setState({...newState});
+    }
+
+    optionOnchange(event, name){
+        let checkbox = event.target;
+        let newState = {...this.state};
+        newState[name] = checkbox.checked;
+
+        let selectAll = true;
+
+        for(let option of this.props.options){
+            if(!newState[option]){
+                selectAll = false;
+            }
+        }
+
+        newState['Select All'] = selectAll;
+
+        this.setState(newState);
+        console.log((checkbox.checked?'Check ':'Unchecked ') + name);
+    }
+
+    render(){
+        return (
+            <Grid container>
+                <Grid item xs={12}>
+                    <Checkbox checked={this.state['Select All']} color={"primary"} onChange={this.selectAll} />All
+                </Grid>
+                {
+                    this.props.options.map(item => (
+                        <Grid item xs={12}>
+                            <Checkbox checked={this.state[item]} color={"primary"} onChange={(event) => this.optionOnchange(event,item)}/>{item}
+                        </Grid>
+                    ))
+                }
+            </Grid>
+        )
+    }
+}
+
+export default ListFilter;
