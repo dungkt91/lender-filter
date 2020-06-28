17592272a99059699f8c3073297016c5d11da67e
Vu Van Dung <vandungktvn@gmail.com>
2020-06-28 22:59:50 +0700

    Updates

--- precache-manifest.ab2d4533e026af56a1f675b2a9dd55f5.js
+++ precache-manifest.ab2d4533e026af56a1f675b2a9dd55f5.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ec8abc1bbbad306d8901170e045ac00f",
+    "revision": "4053565fbadf260f86b050e997480bc3",
     "url": "/lender-filter/index.html"
   },
   {
-    "revision": "afe963710e0fee21e9c2",
+    "revision": "778df8d58fe4563e12fb",
     "url": "/lender-filter/static/css/2.9929f938.chunk.css"
   },
   {
-    "revision": "9538ac285092f0ef0143",
+    "revision": "a69650aed515fb1009e9",
     "url": "/lender-filter/static/css/main.4ce0d9e0.chunk.css"
   },
   {
-    "revision": "afe963710e0fee21e9c2",
-    "url": "/lender-filter/static/js/2.4b782575.chunk.js"
+    "revision": "778df8d58fe4563e12fb",
+    "url": "/lender-filter/static/js/2.ae4b042d.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/lender-filter/static/js/2.4b782575.chunk.js.LICENSE.txt"
+    "url": "/lender-filter/static/js/2.ae4b042d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9538ac285092f0ef0143",
-    "url": "/lender-filter/static/js/main.6a76f420.chunk.js"
+    "revision": "a69650aed515fb1009e9",
+    "url": "/lender-filter/static/js/main.c6bd0a28.chunk.js"
   },
   {
     "revision": "1baf37d3d1dd314a8ca0",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lender-filter/precache-manifest.a3ab2d00fcc8a7ff54f003e896f4b9da.js"
+  "/lender-filter/precache-manifest.ab2d4533e026af56a1f675b2a9dd55f5.js"
 );
 
 self.addEventListener('message', (event) => {
