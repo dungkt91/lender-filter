a13c1a82fcfc5217a9f8fe3ef34d0c544c0878c5
Vu Van Dung <vandungktvn@gmail.com>
2020-07-03 22:48:33 +0700

    Make car gallery images center

--- src/CarShowElement.js
+++ src/CarShowElement.js
@@ -6,6 +6,7 @@ import CarCalculation from "./CarCalculation";
 import CarImagesGallery from "./CarImagesGallery";
 import CarShowElementButtons from "./CarShowElementButtons";
 import Grid from "@material-ui/core/Grid";
+import Utils from './Utils';
 
 export default class CarShowElement extends React.Component{
     excludeCarDetailFields(fieldName){
@@ -54,21 +55,21 @@ export default class CarShowElement extends React.Component{
 
     convertToImages(carDetail){
         console.log(carDetail);
-
+        let carDetailsImages = carDetail["images"];
+        Utils.sortImages(carDetailsImages);
         let images = [];
-        for(let image of carDetail["images"]){
-            console.log(image);
+
+        for(let image of carDetailsImages){
             images.push({original:image["src"], thumbnail:image["src"]});
         }
 
-        console.log(images);
         return images;
     }
 
     render() {
         return (
             <Grid container spacing={2}>
-                <Grid item xs={12} lg={7}>
+                <Grid item xs={12} lg={7} style={{display: 'flex', justifyContent:"center", alignItems: "center"}}>
                     <CarImagesGallery images={this.convertToImages(this.props.details)}/>
                 </Grid>
                 <Grid item xs={12} lg={5}>
