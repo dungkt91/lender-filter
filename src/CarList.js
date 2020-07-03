2ab8a3d03817d7e9840be45219a5ed226b920e3e
Vu Van Dung <vandungktvn@gmail.com>
2020-07-03 11:19:04 +0700

    Enhance mobile version for car list: add navigation buttons

--- src/CarList.js
+++ src/CarList.js
@@ -4,13 +4,13 @@ import Grid from "@material-ui/core/Grid";
 import IconButton from '@material-ui/core/IconButton';
 import ArrowLeft from '@material-ui/icons/ArrowLeft';
 import ArrowRight from '@material-ui/icons/ArrowRight';
+import FirstPageIcon from '@material-ui/icons/FirstPage';
+import LastPageIcon from '@material-ui/icons/LastPage';
 import './CarList.css';
 
 export default class CarList extends React.Component {
     constructor(props) {
         super(props);
-
-        let noCars = this.props.detailsList.length  == 0;
 
         let selectedCarIndex = 0;
 
@@ -20,25 +20,39 @@ export default class CarList extends React.Component {
 
         this.state = {
             selectedCarIndex:selectedCarIndex,
-            previousBtnDisabled:true,
-            nextBtnDisabled:noCars
+            firstBtnDisabled:this.props.initSelectedCarIndex == 0,
+            previousBtnDisabled:this.props.initSelectedCarIndex == 0,
+            nextBtnDisabled:this.props.initSelectedCarIndex == this.props.detailsList.length - 1,
+            lastBtnDisabled:this.props.initSelectedCarIndex == this.props.detailsList.length - 1
         }
 
         this.next = this.next.bind(this);
         this.previous = this.previous.bind(this);
         this.carOnClick = this.carOnClick.bind(this);
+        this.first = this.first.bind(this);
+        this.last = this.last.bind(this);
     }
 
     updateBtnDisabled(selectedCarIndex){
         return {
+            firstBtnDisabled: selectedCarIndex == 0,
             previousBtnDisabled:selectedCarIndex == 0,
-            nextBtnDisabled:(selectedCarIndex == this.props.detailsList.length - 1)
+            nextBtnDisabled:(selectedCarIndex == this.props.detailsList.length - 1),
+            lastBtnDisabled: (selectedCarIndex == this.props.detailsList.length - 1)
         };
     }
 
     callOnChange = (carIndex) => {
         if (this.props.onChange)
             this.props.onChange(this.props.detailsList[carIndex]);
+    }
+
+    first(){
+        if (this.state.selectedCarIndex > 0){
+            let newCarIndex = 0;
+
+            this.setState({selectedCarIndex: newCarIndex, ...this.updateBtnDisabled(newCarIndex)}, this.callOnChange(newCarIndex));
+        }
     }
 
     previous(){
@@ -57,8 +71,64 @@ export default class CarList extends React.Component {
         }
     }
 
+    last(){
+        if (this.state.selectedCarIndex < this.props.detailsList.length - 1){
+            let newCarIndex = this.props.detailsList.length - 1;
+
+            this.setState({selectedCarIndex: newCarIndex, ...this.updateBtnDisabled(newCarIndex)}, this.callOnChange(newCarIndex));
+        }
+    }
+
     carOnClick(carIndex){
         this.setState({selectedCarIndex:carIndex}, this.callOnChange(carIndex));
+    }
+
+    renderFullVersion(startCarIndex, endCarIndex){
+        return (
+            <>
+                <Grid container>
+                    {
+                        this.props.detailsList.slice(startCarIndex, endCarIndex + 1).map((details, index) =>
+                        <Grid item xs={1} style={{padding:10}}>
+                            <div className={(startCarIndex + index == this.state.selectedCarIndex)?"selected_car":""}>
+                                <Car size={"sm"} details={details} onClick={(event, carDetails) => this.carOnClick(startCarIndex + index)}/>
+                            </div>
+                        </Grid>
+                        )
+                    }
+                    <Grid item xs={1} />
+                    <Grid item xs={2}>
+                        <div style={{display:'flex', width:'100%', height:'100%', alignItems: 'center', justifyContent:"center"}}>
+                            <IconButton disabled={this.state.previousBtnDisabled} variant="contained" onClick={this.previous} size={"medium"} className={"full_version navigation_btn prev_btn " + (this.state.previousBtnDisabled?"disabled":"enabled")}><ArrowLeft /> Prev</IconButton>
+                            <IconButton disabled={this.state.nextBtnDisabled} variant="contained" onClick={this.next} size={"medium"} className={"full_version navigation_btn next_btn " + (this.state.nextBtnDisabled?"disabled":"enabled")}>Next <ArrowRight /></IconButton>
+                        </div>
+                    </Grid>
+                </Grid>
+            </>
+        )
+    }
+
+    renderOnlyButtonsVersion(){
+        return (
+            <Grid container>
+                <Grid item xs={2}>
+                    <IconButton disabled={this.state.firstBtnDisabled} onClick={this.first} className={"navigation_btn firt_btn " + (this.state.firstBtnDisabled?"disabled":"enabled")}><FirstPageIcon />First</IconButton>
+                </Grid>
+                <Grid item xs={1}/>
+                <Grid item xs={2}>
+                    <IconButton disabled={this.state.previousBtnDisabled} variant="contained" onClick={this.previous} size={"medium"} className={"navigation_btn prev_btn " + (this.state.previousBtnDisabled?"disabled":"enabled")}><ArrowLeft /> Prev</IconButton>
+                </Grid>
+                <Grid item xs={2}>
+                </Grid>
+                <Grid item xs={2}>
+                    <IconButton disabled={this.state.nextBtnDisabled} variant="contained" onClick={this.next} size={"medium"} className={"navigation_btn next_btn " + (this.state.nextBtnDisabled?"disabled":"enabled")}>Next <ArrowRight /></IconButton>
+                </Grid>
+                <Grid item xs={1}/>
+                <Grid item xs={2}>
+                    <IconButton disabled={this.state.lastBtnDisabled} onClick={this.last} className={"navigation_btn last_btn " + (this.state.lastBtnDisabled?"disabled":"enabled")}>Last<LastPageIcon /></IconButton>
+                </Grid>
+            </Grid>
+        )
     }
 
     render(){
@@ -76,25 +146,10 @@ export default class CarList extends React.Component {
             startCarIndex = this.props.detailsList.length - 9;
         }
 
-        return (
-            <Grid container>
-                {
-                    this.props.detailsList.slice(startCarIndex, endCarIndex + 1).map((details, index) =>
-                        <Grid item xs={1} style={{padding:10}}>
-                            <div className={(startCarIndex + index == this.state.selectedCarIndex)?"selected_car":""}>
-                                <Car size={"sm"} details={details} onClick={(event, carDetails) => this.carOnClick(startCarIndex + index)}/>
-                            </div>
-                        </Grid>
-                    )
-                }
-                <Grid item xs={1} />
-                <Grid item xs={2}>
-                    <div style={{display:'flex', width:'100%', height:'100%', alignItems: 'center', justifyContent:"center"}}>
-                        <IconButton disabled={this.state.previousBtnDisabled} variant="contained" onClick={this.previous} size={"medium"} className={"navigation_btn prev_btn " + (this.state.previousBtnDisabled?"disabled":"enabled")}><ArrowLeft /> Prev</IconButton>
-                        <IconButton disabled={this.state.nextBtnDisabled} variant="contained" onClick={this.next} size={"medium"} className={"navigation_btn next_btn " + (this.state.nextBtnDisabled?"disabled":"enabled")}>Next <ArrowRight /></IconButton>
-                    </div>
-                </Grid>
-            </Grid>
-        )
+        if (this.props.version == "full"){
+            return this.renderFullVersion();
+        }else if(this.props.version == "only_buttons"){
+            return this.renderOnlyButtonsVersion();
+        }
     }
 }
--- src/CarPage.js
+++ src/CarPage.js
@@ -57,29 +57,38 @@ class CarPage extends React.Component {
             lenderInputs = [];
         }
 
+        let carListVersion = "full";
+        let screenIsXs = this.props.screenSize["xsUp"] && !this.props.screenSize["smUp"];
+
+        if (screenIsXs){
+            carListVersion = "only_buttons";
+        }
+
         return (
             <>
-                <ScrollToTop smooth />
+                <ScrollToTop smooth/>
                 <Grid container>
-                    <Grid item xl={2} xs={0} />
+                    <Grid item xl={2} xs={0}/>
                     <Grid item xl={8} xs={12}>
                         <Menubar isBigScreen={this.props.screenSize["mdUp"]}/>
                     </Grid>
                     <Grid item xl={2} xs={0}/>
-                    <Grid item xl={2} xs={0} />
+                    <Grid item xl={2} xs={0}/>
                     <Grid item xl={8} xs={12}>
                         <Grid container item>
                             <Grid item xs={12}>
-                                <a href={"#"} className={"search_results_back_link"}onClick={this.goBack}><BsArrowLeft /> All Results</a>
+                                <a href={"#"} className={"search_results_back_link"}
+                                   onClick={this.goBack}><BsArrowLeft/> All Results</a>
                             </Grid>
-                            {
-                                this.props.screenSize["mdUp"]?
-                                    <Grid item xs={12} className={"car_list"}>
-                                        {results.length > 0?(<CarList initSelectedCarIndex={carIndex} detailsList={results} onChange={this.selectCarInCarList}/>):null}
-                                    </Grid>:null
-                            }
+                            <Grid item xs={12} className={"car_list"}>
+                                {results.length > 0 ? (<CarList version={carListVersion} initSelectedCarIndex={carIndex}
+                                                                detailsList={results}
+                                                                onChange={this.selectCarInCarList}/>) : null}
+                            </Grid>
                             <Grid item xs={12} className={"car_detail"}>
-                                {carDetailsAvailable?<CarShowElement filtersInputs={lenderInputs} lenderData={getLenderData()} details={this.state.carDetails}/>:'Not found'}
+                                {carDetailsAvailable ?
+                                    <CarShowElement filtersInputs={lenderInputs} lenderData={getLenderData()}
+                                                    details={this.state.carDetails}/> : 'Not found'}
                             </Grid>
                         </Grid>
                     </Grid>
