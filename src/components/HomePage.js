35ec3d57c5b6c6e5630afbe016ed9174d31c5f3e
Vu Van Dung <vandungktvn@gmail.com>
2020-07-09 23:03:27 +0700

    Update style

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,21 +5,26 @@ import LeftPanel from "./LeftPanel";
 import CarShow from "./CarShow";
 import LenderInputModalDialog from "./LenderInputModalDialog";
 import React from "react";
+import "./HomePage.css";
 
 class HomePage extends React.Component {
     render(){
         return (<>
             <ScrollToTop smooth/>
             <Grid container className={"app"}>
-                <Grid item xs={12}>
+                <Grid item xs={12} className={"menu_bar"}>
                     <MenuBar />
                 </Grid>
-                <Grid item xs={12} md={3}>
-                    <LeftPanel/>
-                </Grid>
-                <Grid item xs={12} md={9}>
-                    <CarShow/>
-                    <LenderInputModalDialog/>
+                <Grid item xs={12} className={"content"}>
+                    <Grid container>
+                        <Grid item xs={12} md={3}>
+                            <LeftPanel/>
+                        </Grid>
+                        <Grid item xs={12} md={9}>
+                            <CarShow/>
+                            <LenderInputModalDialog/>
+                        </Grid>
+                    </Grid>
                 </Grid>
             </Grid>
         </>);
